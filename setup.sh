d5295ef166f9ddd20fa692a0bdce18ccdde63cac
Dipabali_Halder <85602238+DipabaliHalder@users.noreply.github.com>
2022-06-16 15:40:46 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]
+base="light"
+primaryColor="#4d9e85"
+secondaryBackgroundColor="#efba95"
+textColor="#6f244c"
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" >> ~/.streamlit/config.toml
+" > ~/.streamlit/config.toml
